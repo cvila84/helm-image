cefd6707fcb14c672f13bba21d4eed053713b032
Christophe VILA <christophe.vila@thalesgroup.com>
2020-03-16 16:54:27 +0100

    initial revision

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Helm Image
+
+![helm-spray](https://cvila84.github.io/helm-image/assets/logo.png)
--- main.go
+++ main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"github.com/spf13/cobra"
+	"os"
+)
+
+type imageCmd struct {
+	chartName string
+}
+
+var globalUsage = `
+This command searches for docker images referenced in a chart.
+`
+
+var version = "SNAPSHOT"
+
+func newImageCmd(args []string) *cobra.Command {
+	return nil
+}
+
+func main() {
+	cmd := newImageCmd(os.Args[1:])
+	if err := cmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
